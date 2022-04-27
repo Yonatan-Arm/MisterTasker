3bdbfe27786be363b3900fcb1d10a8693a87ae62
Yonatan Arm <yoniarm123@gmail.com>
2022-04-27 16:05:40 +0300

    edit-page

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
 import { TaskPreview } from "./TaskPreview";
 
 export function TaskList({ tasks, onRemoveTask, history }) {
     
     return (
+        <div>
+         <Link to={`/task/edit/`}>Add task</Link> 
         <section className="task-list simple-cards-grid">
             {tasks.map(task =>
                 <TaskPreview onRemoveTask={onRemoveTask} key={task._id} task={task} />
             )}
         </section>
+        </div>
     )
 }
 
